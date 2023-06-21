3506b1934b132937a49be942f5a1a46e9a62a070
Jaiditya Batra <jaiditya7318@gmail.com>
2023-06-21 23:09:12 +0530

    Initial Commit

---
+++ Hacker News App/Hacker_News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News_AppApp.swift
+//  Hacker News App
+//
+//  Created by Jaiditya Batra on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
